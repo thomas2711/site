91b6258ee17f0ec779991116a7e41064ed98bf44
thomasedw <ter1@williams.edu>
2017-08-14 11:59:29 -0400

    Added movie list app

--- resume/urls.py
+++ resume/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'resume'
 urlpatterns = [
     url('^$', views.index, name="index"),
     url('^contact_$', views.contact_, name="contact_"),
--- website/settings.py
+++ website/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'resume.apps.ResumeConfig',
+    'movies.apps.MoviesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
