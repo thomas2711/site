449f0f5de5f629e16b5e5db5cccb61e060f78716
thomasedw <ter1@williams.edu>
2017-08-17 22:50:07 -0400

    Enable admin

--- website/urls.py
+++ website/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^', include('resume.urls')),
     url(r'^movielist/', include('movies.urls')),
-    #url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 ]
